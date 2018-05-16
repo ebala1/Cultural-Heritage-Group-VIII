57ed799b538e407549ffba2ddcdb52bbb40f71a6
ebala1 <34252455+ebala1@users.noreply.github.com>
2018-05-16 12:21:04 +0200

    Add files via upload

---
+++ navigation.py
@@ -0,0 +1,14 @@
+def menu():
+while True():
+print(
+“Wählen Sie ein Bild aus”, 
+“(1) Bild untersuchen”, 
+“(2) Beenden”)
+choice = int(input(“Auswahl”))
+if choice == 2:
+untersuche_Bild()
+elif choice ==2:
+sys.exit(0)
+else:
+print(“Bitter nur 1 oder 2 eingeben”)
+
